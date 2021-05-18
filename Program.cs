e2bf6b24bee7865c9618bd3d0ab287599f767831
Andres Sainz <andresmsainz@gmail.com>
2021-05-17 22:26:42 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace FileWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FileWrite!");
+
+            string file = @"C:\temp\names.txt";
+            StreamWriter sw = new StreamWriter(file,true);
+
+            sw.WriteLine("Starsky");
+            sw.WriteLine("Hutch");
+            sw.Close();
+
+        }
+    }
+}
